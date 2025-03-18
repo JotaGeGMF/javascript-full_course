e2f8881632f8e15903adfb19292e72b8b53854c3
JotaGeGMF <jggmf2004@gmail.com>
2025-03-18 08:12:06 -0300

    Explicação sobre o uso do console.log e dos comentarios

---
+++ aula02.js
@@ -0,0 +1,20 @@
+//USO DO CONSOLE.LOG E COMENTARIOS:
+
+//O console.log é usado para mostrar algo dentro do console, funciona da mesma forma que o System.out.println do Java e o printf do Python.
+console.log("Hello World")
+console.log(10*25)
+console.log(10/2)
+
+let myName = 'João Gabriel'
+
+console.log('Meu nome é:' + myName)
+console.log(`Meu nome é: ${myName}`)
+//Já os comentários dentro do javascript, podemos ter eles em linha ou em bloco
+
+//let x = 10   -> comentário de linha
+/*
+function myVarSum(a,b){
+    return a + b
+}
+este é um comentário de bloco
+*/
